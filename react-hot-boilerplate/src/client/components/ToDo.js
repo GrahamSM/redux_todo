0bb425eacf609697276ad2b8038b108cd820c44f
graham <graham.sm94@gmail.com>
2016-08-20 23:01:56 -0400

    Updated button action

--- react-hot-boilerplate/src/client/components/ToDo.js
+++ react-hot-boilerplate/src/client/components/ToDo.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const ToDo = React.createClass({
-  render(){
-    debugger;
-    return(
-      <div>
-        <button onClick={this.props.addToDo.bind(null, 'hello')}>Add To-Do</button>
-      </div>
-    )
-  }
-});
 
-export default ToDo;
+export default class Checkout extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    _todoClick = () =>{
+      let text = document.getElementsByClassName('text')[0].value
+      this.props.addToDo(text)
+    }
+    render(){
+      return(
+        <div>
+          <input className="text" name="FirstName"></input>
+          <button onClick={this._todoClick}>Add To-Do</button>
+        </div>
+      )
+    }
+}
 
 // TODO: Why do we have to use bind null?
